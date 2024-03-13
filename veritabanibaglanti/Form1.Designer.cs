a77029160454c9fe1b72fa50a3f4f4930dafcf13
Emirhan Eren <emirhanneren24@gmail.com>
2024-03-13 22:51:01 +0300

    Veri Tabanı Bağlantısı

---
+++ veritabanibaglanti/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace veritabanibaglanti
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(621, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(670, 323);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(580, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Veri Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(465, 341);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(465, 369);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(580, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Yenile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(399, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ad Girin:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(379, 372);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Soyad Girin:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(678, 341);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Veri Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(678, 372);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "XML e Gönder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(465, 398);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(391, 401);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Kimlik Gir:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(580, 400);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(199, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "DGV to XML";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(603, 323);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(580, 430);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(199, 24);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "XML to Listview";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1303, 523);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ veritabanibaglanti/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.OleDb; //Veritabanı kütüphanesini ekliyoruz
+using System.Xml; //XML kütüphanesi
+
+namespace veritabanibaglanti
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection baglanti = new OleDbConnection(); //Kullanacağımız ögeleri global olarak tanımlamamız gerekiyor. Aşağıda kullanmak için
+        OleDbCommand komut = new OleDbCommand();
+        OleDbDataAdapter da1 = new OleDbDataAdapter();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            vericek(); //Okunabilirlik için bu yüzden bu şekilde kullanıldı.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Veri tabanına veri kaydetmek
+            komut = new OleDbCommand("INSERT INTO ogrenci (Ad, Soyad) VALUES (@ad, @soyad)", baglanti);
+            komut.Parameters.AddWithValue("@ad", textBox1.Text); //Kullanılacak olan değişkenlerin ne olduğunu tanımladık.
+            komut.Parameters.AddWithValue("@soyad", textBox2.Text);
+
+            try
+            {
+                baglanti.Open(); // veri tabanını açıyoruz
+                komut.ExecuteNonQuery(); // verileri kaydetme
+                MessageBox.Show("Veri başarıyla eklendi.");
+            }
+            catch (Exception ex) //Hatayı yakalamak için
+            {
+                MessageBox.Show("Veri eklenirken bir hata oluştu: " + ex.Message);
+            }
+            finally
+            {
+                baglanti.Close(); // açık kalması iyi değil, bu yüzden kapatıyoruz
+            }
+        }
+
+        private void vericek()
+        {
+            //Asıl bağlantı komutumuz
+            string baglan, sorgu;
+            baglan = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Application.StartupPath + "\\Database2.mdb";//Provider:Sağlayıcı
+            sorgu = "select * from ogrenci";
+            baglanti = new OleDbConnection(baglan); //Bağlantıyı yukarıda tanımladık fakat boş oldupu için burda yukarıdaki kodu execute ettik.
+            da1 = new OleDbDataAdapter(sorgu, baglanti); //Geçici olarak da e verileri kaydettik.
+            DataSet al = new DataSet(); //dataset ise kalıcı olarak veriyi alır, datagridview veriyi burdan çeker.
+            da1.Fill(al, "abc"); //Dataadapter dan veriyi alıp dataset e (da1-----al)
+            dataGridView1.DataSource = al.Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vericek(); //Veritabanını tekrar açar bir nevi yenilemiş olur.
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            komut = new OleDbCommand("DELETE FROM ogrenci WHERE Kimlik=@Kimlik", baglanti);
+            komut.Parameters.AddWithValue("@Kimlik", dataGridView1.CurrentRow.Cells[0].Value); //Dgv deki seçili olan hücrenin ilk hücresinindeki veri kimlik değişkenine eşit oldu.
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            XmlDocument dosya = new XmlDocument();
+            dosya.Load(Application.StartupPath + "\\deneme.xml");
+
+            XmlElement ogrenci = dosya.CreateElement("ogrenci");
+            ogrenci.SetAttribute("Kimlik", textBox3.Text);
+
+            XmlNode ad = dosya.CreateNode(XmlNodeType.Element, "Ad", "");
+            ad.InnerText = textBox1.Text;
+            ogrenci.AppendChild(ad);
+
+            XmlNode soyad = dosya.CreateNode(XmlNodeType.Element, "Soyad", "");
+            soyad.InnerText = textBox2.Text;
+            ogrenci.AppendChild(soyad);
+
+            dosya.DocumentElement.AppendChild(ogrenci);
+            dosya.Save(Application.StartupPath + "\\deneme.xml");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            XmlDocument dosyaa = new XmlDocument();
+            dosyaa.Load(Application.StartupPath + "\\deneme.xml");
+
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++) //Kaç adet veri varsa o kadar döndürecek
+            {
+                XmlElement ogrenci = dosyaa.CreateElement("ogrenci");
+                ogrenci.SetAttribute("Kimlik", dataGridView1.Rows[i].Cells[0].Value.ToString());
+
+                XmlNode ad = dosyaa.CreateNode(XmlNodeType.Element, "Ad", "");
+                ad.InnerText = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                ogrenci.AppendChild(ad);
+
+                XmlNode soyad = dosyaa.CreateNode(XmlNodeType.Element, "Soyad", "");
+                soyad.InnerText = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                ogrenci.AppendChild(soyad);
+
+                dosyaa.DocumentElement.AppendChild(ogrenci);
+                dosyaa.Save(Application.StartupPath + "\\deneme.xml");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //ÇOK ÖNEMLİ
+
+            list();
+
+            XmlDocument dosya = new XmlDocument();
+            dosya.Load(Application.StartupPath + "\\deneme.xml");
+
+            XmlNodeList b = dosya.SelectNodes("ogrenciler/ogrenci"); //b yi xml deki yeri olarak düşün, a ya tanımladığımız verileri b'ye yani xml deki yerine kaydediyoruz.
+            foreach (XmlNode a in b)
+            {
+                string kimlik = a.Attributes["Kimlik"].Value; //PK olduğu için attributes kullandık, alttakiler pk değil.
+
+                string ad = a["Ad"].InnerText;
+
+                string soyad = a["Soyad"].InnerText;
+
+                ListViewItem item = new ListViewItem(kimlik);
+
+                item.SubItems.Add(ad);
+
+                item.SubItems.Add(soyad);
+
+                listView1.Items.Add(item);
+
+
+            }
+        }
+
+        private void list()
+        {
+            //Görünüş İçin Önemli
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            listView1.Columns.Add("Kimlik");
+            listView1.Columns.Add("Ad");
+            listView1.Columns.Add("Soyad");
+        }
+    }
+}
